ef164c77d83822f88d0fdf06782031a25d305e30
fraser addison <fraseraddison@gmail.com>
2014-02-07 02:58:37 -0800

    added two new files to test rm/mr-movl independently. so far its not working. the program does not complete

--- src/arch/y86/machine/seq/student/mrmovl.s
+++ src/arch/y86/machine/seq/student/mrmovl.s
@@ -1,8 +1,8 @@
 .pos 0x1000
 start:	irmovl $0, %eax
 		irmovl $6, %ecx
-		rmmovl %ecx, a(%eax,4)
 		mrmovl a(%eax,4), %edx
+		xorl %ecx, %ecx
 
 .pos 0x2000
 a:		.long 0
--- src/arch/y86/machine/seq/student/rmmovl.s
+++ src/arch/y86/machine/seq/student/rmmovl.s
@@ -2,7 +2,7 @@
 start:	irmovl $0, %eax
 		irmovl $6, %ecx
 		rmmovl %ecx, a(%eax,4)
-		mrmovl a(%eax,4), %edx
+		xorl %ecx, %ecx
 
 .pos 0x2000
 a:		.long 0
